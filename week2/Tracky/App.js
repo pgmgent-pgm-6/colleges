5376f84baf6a1a6c8f814f5374e55ef23b1fefb4
Michael Vanderpoorten <michael@yungpanda.com>
2022-12-02 10:08:32 +0100

    Week 2 - Navigation

--- week2/Tracky/App.js
+++ week2/Tracky/App.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 import AppContainer from "./src/app/Components/Shared/App/AppContainer";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { Variables } from "./src/app/style";
+import AppNavigator from "./src/app/Navigators/AppNavigator";
 
 const AppTheme = {
   ...DefaultTheme,
@@ -20,6 +21,7 @@ export default function App() {
   return (
     <AppContainer>
       <NavigationContainer theme={AppTheme}>
+        <AppNavigator />
         <StatusBar style="dark" />
       </NavigationContainer>
     </AppContainer>
