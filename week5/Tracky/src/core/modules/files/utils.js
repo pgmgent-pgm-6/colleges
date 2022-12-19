4b8f4ed1301749e5c99214a93d1ffd9a57b2d46c
Michael Vanderpoorten <michael@yungpanda.com>
2022-12-19 10:05:48 +0100

    Comments

--- week5/Tracky/src/core/modules/files/utils.js
+++ week5/Tracky/src/core/modules/files/utils.js
@@ -1,5 +1,6 @@
 import { supabase } from "../../api/supabase";
 
+// get public url from Supabase Storage. This is based on the settings when Supabase is initialized
 export const getPublicUrl = (bucket, filename) => {
   const { data, error } = supabase.storage.from(bucket).getPublicUrl(filename);
 
